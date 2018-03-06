274aa4566e8802095c2df3a71f2338013f19dc00
Andris <aundiks@gmail.com>
2018-03-06 22:55:57 +0000

    All good, man

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,43 @@
+
+
+def return_10
+	return 10
+end
+
+def add(a,b)
+	a + b
+end
+
+def subtract a,b
+	a-b
+end
+
+def multiply a,b
+	a*b
+end
+
+def divide a,b
+	a/b
+end
+
+def length_of_string string 
+	string.length
+end
+
+def join_string string1, string2
+	string1 << string2
+end
+
+def add_string_as_number a,b 
+	a.to_i + b.to_i
+end
+
+def number_to_full_month_name num 
+	months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+	months[num-1]
+end
+
+def number_to_short_month_name num 
+	shortMonth = number_to_full_month_name(num)
+	shortMonth.slice(0,3)
+end
